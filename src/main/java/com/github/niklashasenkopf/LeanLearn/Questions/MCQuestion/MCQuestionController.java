301709060af0750005681c412e0abde0ef5e6993
Niklas Hasenkopf <n.hasenkopf@stud.unibas.ch>
2025-09-16 12:40:19 +0200

    Add support for generating multiple-choice quizzes from uploaded files. Updated controller, service, and prompt creator to handle quiz generation. Introduced `MCQuizDTO` for quiz responses.

--- src/main/java/com/github/niklashasenkopf/LeanLearn/Questions/MCQuestion/MCQuestionController.java
+++ src/main/java/com/github/niklashasenkopf/LeanLearn/Questions/MCQuestion/MCQuestionController.java
@@ -2,7 +2,6 @@ package com.github.niklashasenkopf.LeanLearn.Questions.MCQuestion;
 
 import java.io.IOException;
 import java.util.List;
-import java.util.logging.Logger;
 
 import com.github.niklashasenkopf.LeanLearn.Questions.MCQuestion.models.MCQuestion;
 import com.github.niklashasenkopf.LeanLearn.Questions.MCQuestion.models.MCQuestionCreateRequestDTO;
